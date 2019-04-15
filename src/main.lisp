f0b159fa11764a95044ec6b04a74036eccd24477
Manfred Bergmann <mb@software-by-mabe.com>
2019-04-15 20:29:44 +0200

    Added turenin left an right with commands.

--- src/main.lisp
+++ src/main.lisp
@@ -1,14 +1,42 @@
 (defpackage cl-mars-rover
   (:use :cl)
-  (:export :rover-init))
+  (:export :init-rover
+           :execute-cmds))
 (in-package :cl-mars-rover)
 
 (defparameter *coords* '(1 . 1))
-(defparameter *heading* 'North)
+(defparameter *heading* "N")
 
-(defun rover-init (coords heading)
-  (progn
-    (setf *coords* coords)
-    (setf *heading* heading))
+(defun init-rover (coords heading)
+  (print "Initializing rover")
+  (setf *coords* coords)
+  (setf *heading* heading)
   (list *coords* *heading*))
 
+(defun turn-left ()
+  (print "turning left...")
+  (cond
+    ((equal *heading* "N") (setf *heading* "W"))
+    ((equal *heading* "W") (setf *heading* "S"))
+    ((equal *heading* "S") (setf *heading* "E"))
+    ((equal *heading* "E") (setf *heading* "N"))
+  ))
+
+(defun turn-right ()
+  (print "turning right...")
+  (cond
+    ((equal *heading* "N") (setf *heading* "E"))
+    ((equal *heading* "W") (setf *heading* "N"))
+    ((equal *heading* "S") (setf *heading* "W"))
+    ((equal *heading* "E") (setf *heading* "S"))
+  ))
+
+(defun execute-cmds (cmds)
+  (flet ((handle-cmd (cmd)
+           (print cmd)
+           (cond
+             ((equal cmd "l") (turn-left))
+             ((equal cmd "r") (turn-right))
+             (t "bar"))))
+    (mapc #'(lambda (cmd) (handle-cmd cmd)) cmds))
+  (list *coords* *heading*))
--- tests/main.lisp
+++ tests/main.lisp
@@ -10,11 +10,37 @@
 ;; NOTE: To run this test file, execute `(asdf:test-system :cl-mars-rover)' in your Lisp.
 
 
-(test rover-init
+(test init-rover
   "Tests for initializing the rover"
-  (is (eq (car (rover-init '(1 . 1) 'North)) '(1 . 1)))
-  (is (eq (car (rover-init '(1 . 2) 'North)) '(1 . 2)))
-  (is (eq (cadr (rover-init '(1 . 1) 'North)) 'North))
-  (is (eq (cadr (rover-init '(1 . 1) 'South)) 'South)))
+  (is (eq (car (init-rover '(1 . 1) "N")) '(1 . 1)))
+  (is (eq (car (init-rover '(1 . 2) "N")) '(1 . 2)))
+  (is (equal (cadr (init-rover '(1 . 1) "N")) "N"))
+  (is (equal (cadr (init-rover '(1 . 1) "S")) "S")))
 
-(run! 'rover-init)
+(test turn-rover-left
+  "Tests for turning the rover left with changing it's heading"
+  (init-rover '(1 . 1) "N")
+  (is (equal (cadr (execute-cmds '("l"))) "W"))
+  (init-rover '(1 . 1) "N")
+  (is (equal (cadr (execute-cmds '("l" "l"))) "S"))
+  (init-rover '(1 . 1) "N")
+  (is (equal (cadr (execute-cmds '("l" "l" "l"))) "E"))
+  (init-rover '(1 . 1) "N")
+  (is (equal (cadr (execute-cmds '("l" "l" "l" "l"))) "N"))
+  )
+
+(test turn-rover-right
+  "Tests for turning the rover right with changing it's heading"
+  (init-rover '(1 . 1) "N")
+  (is (equal (cadr (execute-cmds '("r"))) "E"))
+  (init-rover '(1 . 1) "N")
+  (is (equal (cadr (execute-cmds '("r" "r"))) "S"))
+  (init-rover '(1 . 1) "N")
+  (is (equal (cadr (execute-cmds '("r" "r" "r"))) "W"))
+  (init-rover '(1 . 1) "N")
+  (is (equal (cadr (execute-cmds '("r" "r" "r" "r"))) "N"))
+  )
+
+(run! 'init-rover)
+(run! 'turn-rover-left)
+(run! 'turn-rover-right)
