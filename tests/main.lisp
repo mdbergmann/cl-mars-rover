7d3f2ec674ef794d2dafc7080bd54f0d41804025
Manfred Bergmann <mb@software-by-mabe.com>
2019-04-16 16:18:01 +0200

    implemented obstacle detection.

--- tests/main.lisp
+++ tests/main.lisp
@@ -12,31 +12,31 @@
 
 (test init-rover
   "Tests for initializing the rover"
-  (is (eq (car (init-rover '(1 . 1) "N" '(10 . 10))) '(1 . 1)))
-  (is (eq (car (init-rover '(1 . 2) "N" '(10 . 10))) '(1 . 2)))
-  (is (equal (cadr (init-rover '(1 . 1) "N" '(10 . 10))) "N"))
-  (is (equal (cadr (init-rover '(1 . 1) "S" '(10 . 10))) "S")))
+  (is (eq (car (init-rover '(1 . 1) "N" '(10 . 10) nil)) '(1 . 1)))
+  (is (eq (car (init-rover '(1 . 2) "N" '(10 . 10) nil)) '(1 . 2)))
+  (is (equal (cadr (init-rover '(1 . 1) "N" '(10 . 10) nil)) "N"))
+  (is (equal (cadr (init-rover '(1 . 1) "S" '(10 . 10) nil)) "S")))
 
 (defun default-init-rover (heading)
-  (init-rover '(1 . 1) heading '(10 . 10)))
+  (init-rover '(1 . 1) heading '(10 . 10) '((1 . 4))))
 
 (test turn-rover
   "Tests for turning the rover with changing it's heading"
-  (default-init-rover)
+  (default-init-rover "N")
   (is (equal (cadr (execute-cmds '("l"))) "W"))
-  (default-init-rover)
+  (default-init-rover "N")
   (is (equal (cadr (execute-cmds '("l" "l"))) "S"))
-  (default-init-rover)
+  (default-init-rover "N")
   (is (equal (cadr (execute-cmds '("l" "l" "l"))) "E"))
-  (default-init-rover)
+  (default-init-rover "N")
   (is (equal (cadr (execute-cmds '("l" "l" "l" "l"))) "N"))
-  (default-init-rover)
+  (default-init-rover "N")
   (is (equal (cadr (execute-cmds '("r"))) "E"))
-  (default-init-rover)
+  (default-init-rover "N")
   (is (equal (cadr (execute-cmds '("r" "r"))) "S"))
-  (default-init-rover)
+  (default-init-rover "N")
   (is (equal (cadr (execute-cmds '("r" "r" "r"))) "W"))
-  (default-init-rover)
+  (default-init-rover "N")
   (is (equal (cadr (execute-cmds '("r" "r" "r" "r"))) "N"))
   )
 
@@ -44,24 +44,40 @@
   "Tests for moving the rover forward/backward"
   ;; forward
   (default-init-rover "N")
-  (is (equal (cdr (car (execute-cmds '("f" "f")))) 3))
+  (is (= (cdr (car (execute-cmds '("f" "f")))) 3))
   (default-init-rover "E")
-  (is (equal (car (car (execute-cmds '("f" "f")))) 3))
+  (is (= (car (car (execute-cmds '("f" "f")))) 3))
   (default-init-rover "S")
-  (is (equal (cdr (car (execute-cmds '("f" "f")))) 9))
+  (is (= (cdr (car (execute-cmds '("f" "f")))) 9))
   (default-init-rover "W")
-  (is (equal (car (car (execute-cmds '("f" "f")))) 9))
+  (is (= (car (car (execute-cmds '("f" "f")))) 9))
   ;; back
   (default-init-rover "N")
-  (is (equal (cdr (car (execute-cmds '("b" "b")))) 9))
+  (is (= (cdr (car (execute-cmds '("b" "b")))) 9))
   (default-init-rover "E")
-  (is (equal (car (car (execute-cmds '("b" "b")))) 9))
+  (is (= (car (car (execute-cmds '("b" "b")))) 9))
   (default-init-rover "S")
-  (is (equal (cdr (car (execute-cmds '("b" "b")))) 3))
+  (is (= (cdr (car (execute-cmds '("b" "b")))) 3))
   (default-init-rover "W")
-  (is (equal (car (car (execute-cmds '("b" "b")))) 3))
+  (is (= (car (car (execute-cmds '("b" "b")))) 3))
+  )
+
+(test detect-obstacle
+  "Detect obstacles on the way of the rover"
+  (default-init-rover "N")
+  ;; third f is obstacle
+  (is (eq (car (car (execute-cmds '("f" "f" "f")))) 1))
+  (is (eq (cdr (car (execute-cmds '("f" "f" "f")))) 3))
+  (is (eq (caddr (execute-cmds '("f" "f" "f"))) "f"))
+
+  (init-rover '(1 . 4) "N" '(10 . 10) '((1 . 1)))
+  (is (eq (car (car (execute-cmds '("b" "b" "b")))) 1))
+  (is (eq (cdr (car (execute-cmds '("b" "b" "b")))) 2))
+  (is (eq (caddr (execute-cmds '("b" "b" "b"))) "b"))
+  
   )
 
 (run! 'init-rover)
 (run! 'turn-rover)
 (run! 'move-rover)
+(run! 'detect-obstacle)
